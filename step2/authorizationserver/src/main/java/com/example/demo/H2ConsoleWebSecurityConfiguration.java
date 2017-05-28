3bf5108b8d577e8500b4b76a2543ee88d01ce0f8
Andreas Falk <andreas.falk@novatec-gmbh.de>
2017-05-28 20:28:57 +0200

    add step 2

--- step2/authorizationserver/src/main/java/com/example/demo/H2ConsoleWebSecurityConfiguration.java
+++ step2/authorizationserver/src/main/java/com/example/demo/H2ConsoleWebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,20 +14,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * Web security configuration.
  */
-@Profile("!h2console")
+@Profile("h2console")
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class H2ConsoleWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
 
     @Autowired
-    public WebSecurityConfiguration(UserDetailsService userDetailsService) {
+    public H2ConsoleWebSecurityConfiguration(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/h2-console/**")
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and().authorizeRequests().anyRequest().permitAll()
+                .and()
+                .antMatcher("/**")
+                .formLogin().permitAll()
+                .and()
+                .authorizeRequests().anyRequest().fullyAuthenticated();
     }
 
     @Bean
--- step2/authorizationserver/src/main/java/com/example/demo/UserDataInitializer.java
+++ step2/authorizationserver/src/main/java/com/example/demo/UserDataInitializer.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
@@ -12,18 +13,20 @@ import java.util.stream.Stream;
 public class UserDataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserDataInitializer(UserRepository userRepository) {
+    public UserDataInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... strings) throws Exception {
         Stream.of(
                 new User("Hans", "Mustermann",
-                        "user@example.com", "secret"),
+                        "user@example.com", passwordEncoder.encode("secret")),
                 new User("Super", "Admin",
-                        "admin@example.com", "geheim")
+                        "admin@example.com", passwordEncoder.encode("geheim"))
         ).forEach(userRepository::save);
     }
 }
